873b167e3d8b395fa39de3fd9ab77b941c7cbb32
Charly Retana <test@Tests-MacBook-Pro.local>
2025-11-24 07:13:20 -0600

    Fix axios connection url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,14 @@
-// client/src/api/axios.js
 import axios from 'axios';
 
-// Configuración base para conectarnos al Backend
+// LÓGICA DE CONEXIÓN INTELIGENTE:
+// 1. Si existe una variable de entorno en Vercel (VITE_API_URL), usa esa.
+// 2. Si no existe (estás en tu Mac), usa localhost.
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
+
+console.log("Conectando a:", baseURL); // Esto te ayudará a ver en la consola del navegador a dónde apunta
+
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: baseURL,
 });
 
 export default api;
